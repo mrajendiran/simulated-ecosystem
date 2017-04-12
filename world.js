80bc94d4b11be3049fc89f0b04e4dda37ab259c6
Seth Green <seth127@gmail.com>
2017-04-12 15:40:25 -0400

    theyre eating!

--- world.js
+++ world.js
@@ -1,21 +1,21 @@
-var creatures = [];
-var creatures2 = [];
-var creatures3 = [];
+var rabbits = [];
+var wolves = [];
+var grass = [];
 
 function setup() {
   createCanvas(700, 700);
 
   // Initialize creatures
   for (var i = 0; i < 15; i++) {
-    creatures[i] = new Creature(random(width), random(height), "#eaeaea", 20, 3);
+    rabbits[i] = new Creature(random(width), random(height), "#eaeaea", 20, 3);
   }
 
   for (var i = 0; i < 5; i++) {
-    creatures2[i] = new Creature(random(width), random(height), "#000000", 40, 1);
+    wolves[i] = new Creature(random(width), random(height), "#000000", 40, 1);
   }
 
-  for (var i = 0; i < 20; i++) {
-    creatures3[i] = new Creature(random(width), random(height), "green", 10, 0);
+  for (var i = 0; i < 25; i++) {
+    grass[i] = new Creature(random(width), random(height), "green", 10, 0);
   }
 
 }
@@ -23,16 +23,20 @@ function setup() {
 function draw() {
   background('#dee8df');
   // Start population
-  for (var i = 0; i < creatures.length; i++) {
-    creatures[i].run(creatures);
+  for (var i = 0; i < rabbits.length; i++) {
+    rabbits[i].run(rabbits);
+    rabbits[i].eat(grass);
   }
 
-  for (var i = 0; i < creatures2.length; i++) {
-    creatures2[i].run(creatures2);
+  for (var i = 0; i < wolves.length; i++) {
+    wolves[i].run(wolves);
+    wolves[i].eat(grass);
   }
 
-  for (var i = 0; i < creatures3.length; i++) {
-    creatures3[i].run(creatures3);
+  for (var i = 0; i < grass.length; i++) {
+    grass[i].run(grass);
   }
+  
+    //console.log('year end')
 
 }
